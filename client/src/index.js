aace48875c93760af0f0aac5eb13edcbc11f3194
Chacko <chackop@gmail.com>
2020-05-14 15:22:19 +0100

    added navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "gestalt/dist/gestalt.css";
+
+import App from "./components/App";
+import Navbar from "./components/Navbar";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Checkout from "./components/Checkout";
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const Root = () => (
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route component={App} exact path="/" />
+        <Route component={Signin} path="/signin" />
+        <Route component={Signup} path="/signup" />
+        <Route component={Checkout} path="/checkout" />
+      </Switch>
+    </React.Fragment>
+  </Router>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Root />, document.getElementById("root"));
 serviceWorker.unregister();
+
+if (module.hot) {
+  module.hot.accept();
+}
+
