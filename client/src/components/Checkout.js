b2654c76ec5132e007489828c0fb215fc53d3e01
Chacko <chackop@gmail.com>
2020-05-16 00:33:42 +0100

    checkout farm

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { Container, Box, Button, Heading, Text, TextField } from "gestalt";
+// prettier-ignore
+import { Container, Box, Button, Heading, Text, TextField, Modal, Spinner } from "gestalt";
 import ToastMessage from "./ToastMessage";
+import { getCart, calculatePrice } from "../utils";
 
 class Checkout extends React.Component {
   state = {
+    cartItems: [],
     address: "",
     postalCode: "",
     city: "",
     confirmationEmailAddress: "",
     toast: false,
-    toastMessage: ""
+    toastMessage: "",
+    orderProcessing: true,
+    modal: false
   };
+
+  componentDidMount() {
+    this.setState({ cartItems: getCart() });
+  }
 
   handleChange = ({ event, value }) => {
     event.persist();
@@ -24,7 +33,11 @@ class Checkout extends React.Component {
       this.showToast("Fill in all fields");
       return;
     }
+
+    this.setState({ modal: true });
   };
+
+  handleSubmitOrder = () => {};
 
   isFormEmpty = ({ address, postalCode, city, confirmationEmailAddress }) => {
     return !address || !postalCode || !city || !confirmationEmailAddress;
@@ -35,8 +48,11 @@ class Checkout extends React.Component {
     setTimeout(() => this.setState({ toast: false, toastMessage: "" }), 5000);
   };
 
+  closeModal = () => this.setState({ modal: false });
+
   render() {
-    const { toast, toastMessage } = this.state;
+    // prettier-ignore
+    const { toast, toastMessage, cartItems, modal, orderProcessing } = this.state;
 
     return (
       <Container>
@@ -47,60 +63,186 @@ class Checkout extends React.Component {
           shape="rounded"
           display="flex"
           justifyContent="center"
+          alignItems="center"
+          direction="column"
         >
-          {/* Checkout Form */}
-          <form
-            style={{
-              display: "inlineBlock",
-              textAlign: "center",
-              maxWidth: 450
-            }}
-            onSubmit={this.handleConfirmOrder}
-          >
-            {/* Checkout Form Heading */}
-            <Heading color="midnight">Checkout</Heading>
-
-            {/* Shipping Address Input */}
-            <TextField
-              id="address"
-              type="text"
-              name="address"
-              placeholder="Shipping Address"
-              onChange={this.handleChange}
-            />
-            {/* Postal Code Input */}
-            <TextField
-              id="postalCode"
-              type="number"
-              name="postalCode"
-              placeholder="Postal Code"
-              onChange={this.handleChange}
-            />
-            {/* City Input */}
-            <TextField
-              id="city"
-              type="text"
-              name="city"
-              placeholder="City of Residence"
-              onChange={this.handleChange}
-            />
-            {/* Confirmation Email Address Input */}
-            <TextField
-              id="confirmationEmailAddress"
-              type="email"
-              name="confirmationEmailAddress"
-              placeholder="Confirmation Email Address"
-              onChange={this.handleChange}
-            />
-            <button id="stripe__button" type="submit">
-              Submit
-            </button>
-          </form>
+          {/* Checkout Form Heading */}
+          <Heading color="midnight">Checkout</Heading>
+          {cartItems.length > 0 ? (
+            <React.Fragment>
+              {/* User Cart */}
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                direction="column"
+                marginTop={2}
+                marginBottom={6}
+              >
+                <Text color="darkGray" italic>
+                  {cartItems.length} Items for Checkout
+                </Text>
+                <Box padding={2}>
+                  {cartItems.map(item => (
+                    <Box key={item._id} padding={1}>
+                      <Text color="midnight">
+                        {item.name} x {item.quantity} - $
+                        {item.quantity * item.price}
+                      </Text>
+                    </Box>
+                  ))}
+                </Box>
+                <Text bold>Total Amount: {calculatePrice(cartItems)}</Text>
+              </Box>
+              {/* Checkout Form */}
+              <form
+                style={{
+                  display: "inlineBlock",
+                  textAlign: "center",
+                  maxWidth: 450
+                }}
+                onSubmit={this.handleConfirmOrder}
+              >
+                {/* Shipping Address Input */}
+                <TextField
+                  id="address"
+                  type="text"
+                  name="address"
+                  placeholder="Shipping Address"
+                  onChange={this.handleChange}
+                />
+                {/* Postal Code Input */}
+                <TextField
+                  id="postalCode"
+                  type="number"
+                  name="postalCode"
+                  placeholder="Postal Code"
+                  onChange={this.handleChange}
+                />
+                {/* City Input */}
+                <TextField
+                  id="city"
+                  type="text"
+                  name="city"
+                  placeholder="City of Residence"
+                  onChange={this.handleChange}
+                />
+                {/* Confirmation Email Address Input */}
+                <TextField
+                  id="confirmationEmailAddress"
+                  type="email"
+                  name="confirmationEmailAddress"
+                  placeholder="Confirmation Email Address"
+                  onChange={this.handleChange}
+                />
+                <button id="stripe__button" type="submit">
+                  Submit
+                </button>
+              </form>
+            </React.Fragment>
+          ) : (
+            // Default Text if No Items in Cart
+            <Box color="darkWash" shape="rounded" padding={4}>
+              <Heading align="center" color="watermelon" size="xs">
+                Your Cart is Empty
+              </Heading>
+              <Text align="center" italic color="green">
+                Add some brews!
+              </Text>
+            </Box>
+          )}
         </Box>
+        {/* Confirmation Modal */}
+        {modal && (
+          <ConfirmationModal
+            orderProcessing={orderProcessing}
+            cartItems={cartItems}
+            closeModal={this.closeModal}
+            handleSubmitOrder={this.handleSubmitOrder}
+          />
+        )}
         <ToastMessage show={toast} message={toastMessage} />
       </Container>
     );
   }
 }
+
+const ConfirmationModal = ({
+  orderProcessing,
+  cartItems,
+  closeModal,
+  handleSubmitOrder
+}) => (
+  <Modal
+    accessibilityCloseLabel="close"
+    accessibilityModalLabel="Confirm Your Order"
+    heading="Confirm Your Order"
+    onDismiss={closeModal}
+    footer={
+      <Box
+        display="flex"
+        marginRight={-1}
+        marginLeft={-1}
+        justifyContent="center"
+      >
+        <Box padding={1}>
+          <Button
+            size="lg"
+            color="red"
+            text="Submit"
+            disabled={orderProcessing}
+            onClick={handleSubmitOrder}
+          />
+        </Box>
+        <Box padding={1}>
+          <Button
+            size="lg"
+            text="Cancel"
+            disabled={orderProcessing}
+            onClick={closeModal}
+          />
+        </Box>
+      </Box>
+    }
+    role="alertdialog"
+    size="sm"
+  >
+    {/* Order Summary */}
+    {!orderProcessing && (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        direction="column"
+        padding={2}
+        color="lightWash"
+      >
+        {cartItems.map(item => (
+          <Box key={item._id} padding={1}>
+            <Text size="lg" color="red">
+              {item.name} x {item.quantity} - ${item.quantity * item.price}
+            </Text>
+          </Box>
+        ))}
+        <Box paddingY={2}>
+          <Text size="lg" bold>
+            Total: {calculatePrice(cartItems)}
+          </Text>
+        </Box>
+      </Box>
+    )}
+
+    {/* Order Processing Spinner */}
+    <Spinner
+      show={orderProcessing}
+      accessibilityLabel="Order Processing Spinner"
+    />
+    {orderProcessing && (
+      <Text align="center" italic>
+        Submitting Order...
+      </Text>
+    )}
+  </Modal>
+);
 
 export default Checkout;
